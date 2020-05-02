9e4b707a49c0462b698cef5e106a8a5f3155b297
Charlie <charles.j.montoya@gmail.com>
2020-05-01 23:04:59 -0400

    changed truncate to del() in seed files

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,7 +1,7 @@
 exports.seed = function (knex) {
     // Deletes ALL existing entries
     return knex("user")
-        .truncate()
+        .del()
         .then(function () {
             // Inserts seed entries
             return knex("user").insert([
